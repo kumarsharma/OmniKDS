9f18f8cf2db7cf9659730ca7265b0627a6cf2f41
Kumar Sharma <kumar.asom@gmail.com>
2020-01-03 21:12:34 +0530

    Initial Commit

---
+++ OmniKDS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OmniKDS
+//
+//  Created by Kumar Sharma on 03/01/20.
+//  Copyright © 2020 Omni Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OmniKDSTests/OmniKDSTests.swift
@@ -0,0 +1,34 @@
+//
+//  OmniKDSTests.swift
+//  OmniKDSTests
+//
+//  Created by Kumar Sharma on 03/01/20.
+//  Copyright © 2020 Omni Systems. All rights reserved.
+//
+
+import XCTest
+@testable import OmniKDS
+
+class OmniKDSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
